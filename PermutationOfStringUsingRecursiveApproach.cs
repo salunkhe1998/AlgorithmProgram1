27b6417267947a91e4d8c9d8daca34df9045ffa8
Ashwini <salunkheashwini1998@gmail.com>
2023-01-15 12:17:34 +0530

    with static function to return all permutation of string using recursive

---
+++ PermutationOfStringUsingRecursiveApproach.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortingTechniques
+{
+    public class PermutationOfStringUsingRecursiveApproach
+    {
+        public static void RecursiveApproach(String str, String answer)
+        {
+            if (str.Length == 0)
+            {
+                Console.Write(answer + "  ");
+                return;
+            }
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                char ch = str[i];
+                String left_substr = str.Substring(0, i);
+                String right_substr = str.Substring(i + 1);
+                String rest = left_substr + right_substr;
+                RecursiveApproach(rest, answer + ch);
+            }
+        }
+    }
+}
